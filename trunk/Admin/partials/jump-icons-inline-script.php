802648cb89a44a9fd84c20aa70246cd5c992682f
Sascha Hennemann <s.hennemann@rto.de>
2019-08-19 16:47:11 +0200

    Fix issue with jquery

--- trunk/Admin/partials/jump-icons-inline-script.php
+++ trunk/Admin/partials/jump-icons-inline-script.php
@@ -1,19 +1,19 @@
 <script>
   if (localStorage.adminQuickbarDarkmode === 'true') {
-    $('body').addClass('admin-quickbar-is-darkmode');
+    jQuery('body').addClass('admin-quickbar-is-darkmode');
   } else {
-    $('body').removeClass('admin-quickbar-is-darkmode');
+    jQuery('body').removeClass('admin-quickbar-is-darkmode');
   }
 
   if (localStorage.adminQuickbarHideOnWebsite === 'true') {
-    $('.admin-quickbar-jumpicons').css({display: 'none'});
+    jQuery('.admin-quickbar-jumpicons').css({display: 'none'});
   }
 
 
-  $(document).on('click', '.aqb-icon-external', openWindow);
+  jQuery(document).on('click', '.aqb-icon-external', openWindow);
 
   function openWindow() {
-    let url = $(this).next('.dashicons-edit').attr('href') + '&noaqb';
+    let url = jQuery(this).next('.dashicons-edit').attr('href') + '&noaqb';
     window.open(url, '_blank', 'width=700,height=500,left=200,top=100');
   }
 </script>

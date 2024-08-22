52fbdf8c56f27ee0aa0096047aacf0fd24c446af
s.hennemann <s.hennemann@rto.de>
2024-08-22 08:24:55 +0200

    update to gulp 5

--- trunk/gulpfile.js
+++ trunk/gulpfile.js
@@ -1,11 +1,13 @@
+
+
 var gulp = require('gulp'),
   util = require("gulp-util"),//https://github.com/gulpjs/gulp-util
-  sass = require("gulp-sass"),//https://www.npmjs.org/package/gulp-sass
-  autoprefixer = require('gulp-autoprefixer'),//https://www.npmjs.org/package/gulp-autoprefixer
+  sass = require('gulp-sass')(require('sass')),//https://www.npmjs.org/package/gulp-sass
   minifycss = require('gulp-clean-css'),//https://github.com/scniro/gulp-clean-css
   rename = require('gulp-rename'),//https://www.npmjs.org/package/gulp-rename
   concat = require('gulp-concat'),
   uglify = require('gulp-terser');
+
 
 gulp.task('default', function(done) {
   // Admin
@@ -15,7 +17,6 @@ gulp.task('default', function(done) {
 
   gulp.src(sassAdminFiles)
     .pipe(sass({style: 'expanded'}))
-    .pipe(autoprefixer("last 3 version", "safari 5", "ie 8", "ie 9"))
     .pipe(rename({suffix: '.min'}))
     .pipe(minifycss())
     .pipe(gulp.dest('Admin/css'))
